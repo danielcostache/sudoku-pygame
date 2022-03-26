cf2d18dc9c1d75baee1a3984cc11e21306ddb188
Daniel Costache <ccristian2001@gmail.com>
2022-03-26 19:48:17 +0200

    Implemented board class

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from email.policy import default
-from tkinter.tix import CELL
-from types import CellType
 import pygame
 import os
 import numpy as np
@@ -16,31 +13,94 @@ GRID_SIZE = WIDTH/3
 
 FPS = 60
 
-# FUNCTION FOR DRAWING THE GRID
-def draw_grid() -> None:
-    WINDOW.fill((255, 255, 255))
-    for i in range(1, 9):
-        VERT_CELL_BORDER = pygame.Rect(i*CELL_SIZE - 1, 0, 2, HEIGHT)    
-        pygame.draw.rect(WINDOW, (0, 0, 0), VERT_CELL_BORDER)
-        HOR_CELL_BORDER = pygame.Rect(0, i*CELL_SIZE - 1, WIDTH, 2)
-        pygame.draw.rect(WINDOW, (0, 0, 0), HOR_CELL_BORDER)
-    for i in range(1, 3):
-        VERT_GRID_BORDER = pygame.Rect(i*GRID_SIZE -3, 0, 6, HEIGHT)
-        pygame.draw.rect(WINDOW, (0, 0, 0), VERT_GRID_BORDER)
-        HOR_GRID_BORDER = pygame.Rect(0, i*GRID_SIZE - 3, WIDTH, 6)
-        pygame.draw.rect(WINDOW, (0, 0, 0), HOR_GRID_BORDER)
-    for i in range(9):
-        for j in range(9):
-            if default_grid[i][j]:
-                text = FONT.render(str(default_grid[i][j]), 1, (0, 0, 0))
-                WINDOW.blit(text, (i*CELL_SIZE + 30, j*CELL_SIZE + 15))    
+default_grid = np.transpose(np.array([
+        [2, 1, 0, 0, 0, 0, 4, 0, 0],
+        [3, 8, 0, 4, 0, 0, 7, 0, 2],
+        [0, 0, 0, 7, 2, 0, 0, 0, 0],
+        [0, 2, 4, 8, 0, 6, 9, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 2, 0, 3, 5, 4, 0],
+        [0, 0, 0, 0, 5, 8, 0, 0, 0],
+        [9, 0, 3, 0, 0, 4, 0, 2, 8],
+        [0, 0, 8, 0, 0, 0, 0, 5, 7],
+    ]))
 
+class Board:
+    def __init__(self, grid):
+        self.grid = grid
+        pass
+    
+    # FUNCTION FOR DRAWING THE GRID
+    def draw_grid(self) -> None:    
+        WINDOW.fill((255, 255, 255))
+        for i in range(1, 9):
+            VERT_CELL_BORDER = pygame.Rect(i*CELL_SIZE - 1, 0, 2, HEIGHT)    
+            pygame.draw.rect(WINDOW, (0, 0, 0), VERT_CELL_BORDER)
+            HOR_CELL_BORDER = pygame.Rect(0, i*CELL_SIZE - 1, WIDTH, 2)
+            pygame.draw.rect(WINDOW, (0, 0, 0), HOR_CELL_BORDER)
+        for i in range(1, 3):
+            VERT_GRID_BORDER = pygame.Rect(i*GRID_SIZE -3, 0, 6, HEIGHT)
+            pygame.draw.rect(WINDOW, (0, 0, 0), VERT_GRID_BORDER)
+            HOR_GRID_BORDER = pygame.Rect(0, i*GRID_SIZE - 3, WIDTH, 6)
+            pygame.draw.rect(WINDOW, (0, 0, 0), HOR_GRID_BORDER)
+        for i in range(9):
+            for j in range(9):
+                if self.grid[i][j]:
+                    text = FONT.render(str(self.grid[i][j]), 1, (0, 0, 0))
+                    WINDOW.blit(text, (i*CELL_SIZE + 30, j*CELL_SIZE + 15))    
+                    
+    # FUNCTION FOR CHECKING VALIDITY OF AN INPUT
+    def is_valid(self, row: int, col: int, value) -> bool:
+        for i in range(9):
+            if self.grid[row][i] == value:
+                return False
+            if self.grid[i][col] == value:
+                return False
+        i = row // 3
+        j = col // 3
+        for row in range(i*3, i*3 + 3):
+            for col in range(j*3, j*3 + 3):
+                if self.grid[row][col] == value:
+                    return False
+        return True
+    
+    # FUNCTION FOR FINDING A SOLUTION
+    def is_solution(self, i, j) -> bool:
+        while self.grid[i][j]:
+            if i < 8:
+                i += 1
+            elif i == 8 and j < 8:
+                i = 0
+                j += 1
+            elif i == 8 and j == 8:
+                return True
+        pygame.event.pump()
+        for k in range(1, 10):
+            if self.is_valid(i, j, k):
+                self.grid[i][j] = k
+                global x, y
+                x = i
+                y = j           
+                WINDOW.fill((255, 255, 255))
+                self.draw_grid()
+                pygame.display.update()
+                pygame.time.delay(25)
+                if self.is_solution(i, j):
+                    return True
+                else:
+                    self.grid[i][j] = 0
+                WINDOW.fill((0, 0, 0))
+                self.draw_grid()
+                highlight_cell()
+                pygame.display.update()
+                pygame.time.delay(25)
+        return False
+            
 # FUNCTION FOR COMPUTING CARTESIAN COORDINATES
 def coord(position) -> None:
     global x, y
     x = int(position[0] // CELL_SIZE)
     y = int(position[1] // CELL_SIZE)
-    
     
 # FUNCTION FOR HIGHLIGHTING A CELL  
 def highlight_cell() -> None:
@@ -57,58 +117,13 @@ def fill_value(value: int) -> None:
 def mistake_error() -> None:
     text = FONT.render('Wrong answer!', 1, (0, 0, 0,))
     WINDOW.blit(text, (20, 570))
+    
 # FUNCTION FOR RAISING TYPE ERROR TRIGGERED ON INVALID INPUT
 def raise_error() -> None:
     text = FONT.render('Input invalid! Please enter a correct value!', 1, (0, 0, 0))
     WINDOW.blit(text, (20, 570))
-    
-# FUNCTION FOR CHECKING VALIDITY OF INPUT
-def is_valid(matrix, row: int, col: int, value) -> bool:
-    for i in range(9):
-        if matrix[row][i] == value:
-            return False
-        if matrix[i][col] == value:
-            return False
-    i = row // 3
-    j = col // 3
-    for row in range(i*3, i*3 + 3):
-        for col in range(j*3, j*3 + 3):
-            if matrix[row][col] == value:
-                return False
-    return True
-       
-# FUNCTION FOR FINDING A SOLUTION       
-def is_solution(grid, i, j) -> bool:
-    while grid[i][j]:
-        if i < 8:
-            i += 1
-        elif i == 8 and j < 8:
-            i = 0
-            j += 1
-        elif i == 8 and j == 8:
-            return True
-    pygame.event.pump()
-    for k in range(1, 10):
-        if is_valid(grid, i, j, k):
-            grid[i][j] = k
-            global x, y
-            x = i
-            y = j           
-            WINDOW.fill((255, 255, 255))
-            draw_grid()
-            pygame.display.update()
-            pygame.time.delay(25)
-            if is_solution(grid, i, j):
-                return True
-            else:
-                grid[i][j] = 0
-            WINDOW.fill((0, 0, 0))
-            draw_grid()
-            highlight_cell()
-            pygame.display.update()
-            pygame.time.delay(25)
-    return False
 
+# FUNCTION FOR END OF GAME SCREEN
 def result() -> None:
     text = FONT.render('Game finished', 1, (0, 0, 0,))
     pygame.draw.circle(WINDOW, (255, 128, 128), (WIDTH/2, HEIGHT/2), text.get_width())
@@ -118,18 +133,7 @@ def result() -> None:
     
 # CORE GAME LOOP       
 def main() -> None:
-    global default_grid 
-    default_grid = np.transpose(np.array([
-        [2, 1, 0, 0, 0, 0, 4, 0, 0],
-        [3, 8, 0, 4, 0, 0, 7, 0, 2],
-        [0, 0, 0, 7, 2, 0, 0, 0, 0],
-        [0, 2, 4, 8, 0, 6, 9, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 2, 0, 3, 5, 4, 0],
-        [0, 0, 0, 0, 5, 8, 0, 0, 0],
-        [9, 0, 3, 0, 0, 4, 0, 2, 8],
-        [0, 0, 8, 0, 0, 0, 0, 5, 7],
-    ]))
+    game_board = Board(default_grid)    
     clock = pygame.time.Clock()
     run = True
     click = False 
@@ -173,10 +177,15 @@ def main() -> None:
                     res_cond = False
                     error = False
                     solve_cond = False
-                    defaultgrid = np.array([
+                    game_board.grid = np.array([
                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0],    
+# FUNCTION FOR CHECKING VALIDITY OF INPUT
+
+       
+# FUNCTION FOR FINDING A SOLUTION       
+
                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -188,27 +197,17 @@ def main() -> None:
                     res_cond = False
                     error = False
                     solve_cond = False
-                    default_grid = np.transpose(np.array([
-                        [2, 1, 0, 0, 0, 0, 4, 0, 0],
-                        [3, 8, 0, 4, 0, 0, 7, 0, 2],
-                        [0, 0, 0, 7, 2, 0, 0, 0, 0],
-                        [0, 2, 4, 8, 0, 6, 9, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 1, 2, 0, 3, 5, 4, 0],
-                        [0, 0, 0, 0, 5, 8, 0, 0, 0],
-                        [9, 0, 3, 0, 0, 4, 0, 2, 8],
-                        [0, 2, 8, 0, 0, 0, 0, 5, 7],
-                    ]))
+                    game_board.grid = default_grid
         if solve_cond:
-            if not is_solution(default_grid, 0, 0):
+            if not game_board.is_solution(0, 0):
                 error = True
             else:
                 res_cond = True
             solve_cond = False
         if value:
             fill_value(value)
-            if is_valid(default_grid, x, y, value):
-                default_grid[x][y] = value
+            if game_board.is_valid(x, y, value):
+                game_board.grid[x][y] = value
                 click = False
             else:
                 raise_error()
@@ -218,7 +217,7 @@ def main() -> None:
         if res_cond:
             result()
         
-        draw_grid()
+        game_board.draw_grid()
         if click:
             highlight_cell()
         pygame.display.update()
